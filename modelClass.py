f618cb3057293ad332771bb8b05dd18d5c9b27dc
vellaro <r,vella@hotmail.de>
2023-03-10 21:33:38 +0100

    update

--- modelClass.py
+++ modelClass.py
@@ -2,27 +2,36 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class BinaryClassification(nn.Module):
-    def __init__(self, inputFeatures=8):
+    def __init__(self, inputFeatures=8, outputFeatures=2):
         super(BinaryClassification, self).__init__()
+        
+        self.modelName = "BinaryClassification"
+
         # Number of input features is 8.
-        self.layer_1 = nn.Linear(inputFeatures, 64) 
-        self.layer_2 = nn.Linear(64, 64)
-        self.layer_out = nn.Linear(64, 2) 
+        self.layer_1 = nn.Linear(inputFeatures, 16 ) #64
+        #self.layer_2 = nn.Linear(16, 16) #64,64 
+        self.layer_out = nn.Linear(16, outputFeatures) #64
         
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.1) # 0.1
+        #self.dropout = nn.Dropout(p=0.1) # 0.1
         
     def forward(self, inputs):
         x = self.relu(self.layer_1(inputs))
-        x = self.relu(self.layer_2(x))
-        x = self.dropout(x)
+        x# = self.relu(self.layer_2(x))
+        #x = self.dropout(x)
         x = self.layer_out(x)
         
         return x
 
+
+        return modelName
+
 class Net(nn.Module):
-    def __init__(self, inputFeatures=8,hidden1=64, hidden2=128, hidden3=64,hidden4=64,out_features=2):
+    def __init__(self, inputFeatures=8,hidden1=512, hidden2=512, hidden3=512,hidden4=512,out_features=2):
         super().__init__()
+
+        self.modelName = "Net"
+
         self.f_connected1 = nn.Linear(inputFeatures,hidden1)
         self.f_connected2 = nn.Linear(hidden1,hidden2)
         self.f_connected3 = nn.Linear(hidden2,hidden3)
