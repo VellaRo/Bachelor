fca27f6c60f7b1ada024c764dc915a47a9b8c7c9
vellaro <r,vella@hotmail.de>
2023-03-12 22:32:45 +0100

    cleaned

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import torch
-import pandas as pd
-import sklearn.model_selection # train_test_split
-import sklearn.metrics  #  confusion_matrix  accuracy_score
 import torch.nn as nn 
 #Sebastian Class
 import TorchRandomSeed
 
 seed =0
 seedObject = TorchRandomSeed.TorchRandomSeed(seed=1) # was mache ich falsch ?
-
-#from torch import manual_seed
-#manual_seed(seed)
 
 #my classes
 import dataloader
@@ -20,9 +13,6 @@ import utils
 import plotResults
 
 with seedObject:
-
-     # add to classes like with inputFeatures in dataloader
-     # add to classes like with inputFeatures in dataloader
 
     #inputFeatures = 4 #
     droplist = []#pi["BloodPressure", "Pregnancies", "Age", "SkinThickness"]
@@ -35,8 +25,6 @@ with seedObject:
     #device = "cpu"
     lr =0.1 # 0.001 slowed learningrate
 
-    doEval = False
-
     # load data
     trainloader ,evalloader, testloader ,X_train ,X_eval, X_test,  y_train ,y_eval, y_test, inputFeatures, outputFeatures, datasetName, featureNames= dataloader.load_kaggle_diabetes_dataset(batch_size=batch_size , droplist= droplist)
     #trainloader ,evalloader, testloader ,X_train ,X_eval, X_test,  y_train ,y_eval, y_test, inputFeatures, outputFeatures, datasetName, featureNames= dataloader.BreastCancerUCI(batch_size= batch_size, droplist=droplist, test_size=test_size)
@@ -45,7 +33,7 @@ with seedObject:
     #model = modelClass.Net(inputFeatures= inputFeatures, out_features=outputFeatures)
     model= modelClass.BinaryClassification(inputFeatures= inputFeatures, outputFeatures= outputFeatures)
     modelName = model.modelName
-    grads,grads_eval, grads_0, grads_epoch, grads_epoch_0,training_loss_batch, training_loss_epoch, training_acc, test_loss_batch, test_loss_epoch, test_acc, loss_function= train.train_eval(trainloader,evalloader, testloader, model, num_epochs, device, y_train,y_test, lr, doEval= doEval)
+    grads,grads_eval, grads_0, grads_epoch, grads_epoch_0,training_loss_batch, training_loss_epoch, training_acc, test_loss_batch, test_loss_epoch, test_acc, loss_function= train.train(trainloader, model, num_epochs, device, y_train, lr)
 
     train_predictions,test_predictions = utils.calculatePredictions(model ,X_test, X_train , device)
     featureListALL = utils.unpackingFeatureList(inputFeatures , grads)
@@ -60,25 +48,14 @@ with seedObject:
 
     print("ploting...")
     import eval
-    #plotResults.plot_cosine_similarity(dirPath, "cosine_simialarity",model, modelsDirPath)
-    #plotResults.plotWeightSignDifferences(dirPath, "percentageWeightsSignDifference",model, modelsDirPath)
-    #plotResults.plotWeightsMagnitude(dirPath, "averageWeightsMagnitude",model, modelsDirPath)
-    #plotResults.plotL2Distance(dirPath, "L2Distance",model, modelsDirPath)
-    #plotResults.plotWeightTrace(dirPath, "weightTrace",model, modelsDirPath)
-    #plotResults.plotGradientMagnitude(dirPath, "averageGradientMagnitude",featureListALL, perFeature=False)
-    #plotResults.plotGradientMagnitude(dirPath, "GradientMagnitudePerFeature",featureListALL, perFeature=True)
-
-    #plotResults.plotGradientsPerSample(featureListALL, num_epochs, X_train, dirPath, "featureListALLPerSample")
-
-    #plotResults.plot_CM(y_train,y_test,  train_predictions, test_predictions, dirPath)
-    plotResults.plot_Loss_Acc(dirPath,model,modelsDirPath, trainloader,evalloader,testloader, device, loss_function, num_epochs, y_train, y_eval, y_test)
-    #plotResults.plot_features(dirPath, inputFeatures, featureListALL, plotName = "fetureListALL")
-    #if doEval:
-    #    plotReults.plot_features(dirPath, inputFeatures, featureListALL_eval, plotName = "featureListALL_eval")
-    
-    #plotResults.plot_features(dirPath, inputFeatures, featureListALL_0, plotName = "featureListALL_0")
-    ###s#
-    #plotResults.plot_features(dirPath, inputFeatures, featureListALL_epoch, plotName = "fetureListALL_epoch")
-    #plotResults.plot_features(dirPath, inputFeatures, featureListALL_0_epoch, plotName = "fetureListALL_0_epoch")
-
-    
+    plotResults.plotCosineSimilarity(dirPath, "cosine_simialarity",model, modelsDirPath)
+    plotResults.plotWeightSignDifferences(dirPath, "percentageWeightsSignDifference",model, modelsDirPath)
+    plotResults.plotWeightMagnitude(dirPath, "averageWeightsMagnitude",model, modelsDirPath)
+    plotResults.plotL2Distance(dirPath, "L2Distance",model, modelsDirPath)
+    plotResults.plotWeightTrace(dirPath, "weightTrace",model, modelsDirPath)
+    plotResults.plotGradientsPerFeature(dirPath, inputFeatures, featureListALL, plotName = "fetureListALL")
+    plotResults.plotGradientMagnitude(dirPath, "averageGradientMagnitude",featureListALL, perFeature=False)
+    plotResults.plotGradientMagnitude(dirPath, "GradientMagnitudePerFeature",featureListALL, perFeature=True)
+    plotResults.plotGradientsPerSample(featureListALL, num_epochs, X_train, dirPath, "featureListALLPerSample")
+    plotResults.plotConfusionMatrix(y_train,y_test,  train_predictions, test_predictions, dirPath)
+    plotResults.plotLoss_Acc(dirPath,model,modelsDirPath, trainloader,evalloader,testloader, device, loss_function, num_epochs, y_train, y_eval, y_test)
