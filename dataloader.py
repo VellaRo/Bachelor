dcc4fb79957766595a66a484235df589719cbe06
vellaro <r.vella@hotmail.de>
2023-06-07 09:44:25 +0200

    update

--- dataloader.py
+++ dataloader.py
@@ -6,7 +6,9 @@ import numpy as np
 from sklearn.datasets import load_breast_cancer
 
 from sklearn.model_selection import StratifiedKFold
-
+from torch.utils.data import SubsetRandomSampler
+from torch.utils.data import Sampler
+import random
 class StratifiedBatchSampler:
     """
        Stratified batch sampling
@@ -18,7 +20,8 @@ class StratifiedBatchSampler:
             y = y.numpy()
         assert len(y.shape) == 1, 'label array must be 1D'
         n_batches = int(len(y) / batch_size)
-        self.skf = StratifiedKFold(n_splits=n_batches, shuffle=shuffle)
+        print(n_batches)
+        self.skf = StratifiedKFold(n_splits=n_batches, shuffle=shuffle)  
         self.X = torch.randn(len(y),1).numpy()
         self.y = y
         self.shuffle = shuffle
@@ -66,17 +69,16 @@ def preProcessingData(X, y, batch_size= 4, test_size =0.2 ,droplist= []): #, pat
     inputFeatures= 0
 
     # split into train , eval test
-    X_train, X_rem, y_train, y_rem = train_test_split(X,y, test_size =test_size,random_state=1)
-
-    X_eval, X_test, y_eval, y_test = train_test_split(X_rem,y_rem, test_size=0.5, random_state=1)
+    #X_train, X_rem, y_train, y_rem = train_test_split(X,y, test_size =test_size,random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size =test_size,random_state=1)
+    #X_eval, X_test, y_eval, y_test = train_test_split(X_rem,y_rem, test_size=0.5, random_state=1)
 
     #convert them to tensors
     X_train=torch.FloatTensor(X_train.values)
-    X_eval=torch.FloatTensor(X_eval.values)
+    #X_eval=torch.FloatTensor(X_eval.values)
     X_test=torch.FloatTensor(X_test.values)
-
     y_train=torch.LongTensor(y_train.values)
-    y_eval=torch.LongTensor(y_eval.values)
+    #y_eval=torch.LongTensor(y_eval.values)
     y_test=torch.LongTensor(y_test.values)
     X , inputFeatures= dropFeatures(droplist=droplist, X=X, inputFeatures=inputFeatures)
 
@@ -85,32 +87,55 @@ def preProcessingData(X, y, batch_size= 4, test_size =0.2 ,droplist= []): #, pat
 
         mean = torch.mean(inputToNormalize, dim =dim ,keepdim= True)
         variance = torch.var(inputToNormalize, dim =dim ,keepdim= True)
+        #print(variance)
         eps=1e-5
         normalizeed1 = (inputToNormalize - mean) / torch.sqrt(variance+ eps)
  
         return normalizeed1
 
     X_train = normalize(X_train, dim=1)
-    X_eval = normalize(X_eval, dim=1)
+    #X_eval = normalize(X_eval, dim=1)
     X_test = normalize(X_test, dim=1)
 
     # stratisfied sampler
-    train_sampler = StratifiedBatchSampler(y_train, batch_size=batch_size, shuffle=True)
-    eval_sampler = StratifiedBatchSampler(y_eval, batch_size=batch_size, shuffle=True)
-    test_sampler = StratifiedBatchSampler(y_test, batch_size=batch_size, shuffle=False)
-
+    ##print(batch_size)
+    #train_sampler = StratifiedBatchSampler(y_train, batch_size=batch_size, shuffle=True)
+    #eval_sampler = StratifiedBatchSampler(y_eval, batch_size=batch_size, shuffle=True)
+    #test_sampler = StratifiedBatchSampler(y_test, batch_size=batch_size, shuffle=False)
+    ##print(batch_size)
     trainset = list(zip(X_train , y_train))      
-    trainloader = torch.utils.data.DataLoader(trainset, shuffle=False, batch_sampler=train_sampler , num_workers=2)# shuffle false
+    
+    random_indices_test =  data_list = random.sample(range(len(X_test)), len(X_test))
+    random_indices_train =  data_list = random.sample(range(len(X_test)), len(X_test))
+
+    
+    class OrderedListSampler(Sampler):
+        def __init__(self, data_list):
+            self.data_list = data_list
+
+        def __iter__(self):
+            return iter(self.data_list)
+
+        def __len__(self):
+            return len(self.data_list)
+    
+    sampler_test = OrderedListSampler(random_indices_test)
+    sampler_train =OrderedListSampler(random_indices_train)
+    #trainloader = torch.utils.data.DataLoader(trainset, shuffle=False, batch_sampler=train_sampler , num_workers=2)# shuffle false
+    trainloader = torch.utils.data.DataLoader(trainset, shuffle=False, batch_size= batch_size , num_workers=2, sampler= sampler_train)
     print("train:shuffel = False")
-    evalset = list(zip(X_eval , y_eval))      
-    evalloader =  torch.utils.data.DataLoader(evalset, shuffle=False, batch_sampler=eval_sampler , num_workers=2)
+    #evalset = list(zip(X_eval , y_eval))      
+    #evalloader =  torch.utils.data.DataLoader(evalset, shuffle=False, batch_sampler=eval_sampler , num_workers=2)
     print("eval:shuffel = False")
     
     testset = list(zip(X_test , y_test))
-    testloader = torch.utils.data.DataLoader(testset,shuffle=False, batch_sampler=test_sampler , num_workers=2)
+    #testloader = torch.utils.data.DataLoader(testset,shuffle=False, batch_sampler=test_sampler , num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset,shuffle=False, batch_size=batch_size , num_workers=2 ,sampler = sampler_test)
 
 
-    return trainloader ,evalloader, testloader ,X_train ,X_eval, X_test ,  y_train, y_eval, y_test
+    #return trainloader ,evalloader, testloader ,X_train ,X_eval, X_test ,  y_train, y_eval, y_test
+    return trainloader , testloader ,X_train , X_test ,  y_train, y_test , random_indices_train, random_indices_test
+
 
 
 
@@ -133,7 +158,7 @@ def load_kaggle_diabetes_dataset( batch_size= 4, test_size =0.2 ,droplist= []):
     data = pd.read_csv(path) #macht ohne r' problme ?
 
     inputFeatures = len(data.columns) -1 
-    print(inputFeatures)
+    #print(inputFeatures)
 
     outputFeatures =2 
   
@@ -149,9 +174,10 @@ def load_kaggle_diabetes_dataset( batch_size= 4, test_size =0.2 ,droplist= []):
     y = data["Outcome"]
     #y = y[:100]
 
-    trainloader ,evalloader, testloader ,X_train ,X_eval, X_test ,  y_train, y_eval, y_test= preProcessingData(X=X, y=y,batch_size=batch_size, test_size=test_size,droplist=droplist)
-    return trainloader ,evalloader, testloader ,X_train, X_eval, X_test, y_train, y_eval, y_test , inputFeatures, outputFeatures, datasetName, features_names
-
+    #trainloader ,evalloader, testloader ,X_train ,X_eval, X_test ,  y_train, y_eval, y_test= preProcessingData(X=X, y=y,batch_size=batch_size, test_size=test_size,droplist=droplist)
+    #return trainloader ,evalloader, testloader ,X_train, X_eval, X_test, y_train, y_eval, y_test , inputFeatures, outputFeatures, datasetName   trainloader ,evalloader, testloader ,X_train ,X_eval, X_test ,  y_train, y_eval, y_test= preProcessingData(X=X, y=y,batch_size=batch_size, features_names
+    trainloader , testloader ,X_train , X_test ,  y_train, y_test ,random_indices_train, random_indices_test, = preProcessingData(X=X, y=y,batch_size=batch_size, test_size=test_size,droplist=droplist)
+    return trainloader ,random_indices_train, testloader,random_indices_test,X_train , X_test,  y_train , y_test, inputFeatures, outputFeatures, datasetName, features_names
 
 
 def BreastCancerUCI(batch_size= 4, test_size =0.2 ,droplist= []): # 0.2 tset,size  0.2 macght probleme ???ß
@@ -166,7 +192,7 @@ def BreastCancerUCI(batch_size= 4, test_size =0.2 ,droplist= []): # 0.2 tset,siz
     datasetName = "BreastCancerUCI"
     data= load_breast_cancer(as_frame=True)
     features_names =  data.feature_names
-    print(features_names)
+    #print(features_names)
     inputFeatures = 30
     outputFeatures =2
     data = data.frame
@@ -176,8 +202,8 @@ def BreastCancerUCI(batch_size= 4, test_size =0.2 ,droplist= []): # 0.2 tset,siz
     y= data["target"]    
 
     X, inputFeatures = dropFeatures(droplist= droplist, X=X, inputFeatures=inputFeatures)
-    trainloader ,evalloader, testloader ,X_train ,X_eval, X_test ,  y_train, y_eval, y_test = preProcessingData(X=X, y=y,batch_size=batch_size, test_size=test_size,droplist=droplist)
-    return trainloader ,evalloader, testloader ,X_train, X_eval, X_test, y_train, y_eval, y_test , inputFeatures, outputFeatures, datasetName, features_names
+    trainloader, testloader ,X_train , X_test ,  y_train, y_test = preProcessingData(X=X, y=y,batch_size=batch_size, test_size=test_size,droplist=droplist)
+    return trainloader , testloader ,X_train, X_test, y_train, y_test , inputFeatures, outputFeatures, datasetName, features_names
 
 def dryBeanUCI(batch_size= 4, test_size =0.2 ,droplist= []):
     """
@@ -190,17 +216,17 @@ def dryBeanUCI(batch_size= 4, test_size =0.2 ,droplist= []):
     
     """
     path = "/home/rosario/explainable/Bachelor/Diabetes/Data/DryBeanDataset/Dry_Bean_Dataset.csv"
-
+    
     datasetName = "dryBeanUCI"
     inputFeatures = 16
     outputFeatures = 2# 7# 
     data = pd.read_csv(path) 
-
+    features_names =  data.feature_names
     
     X = data.drop('Class' , axis = 1) #independent Feature
     #print(X)
     X = X[:3349]
-    print(X)
+    #print(X)
     X = X.drop('IntClass' , axis = 1) #independent Feature
     #print(X)
     X = X.drop('IntClass1' , axis = 1) #independent Feature
@@ -211,6 +237,6 @@ def dryBeanUCI(batch_size= 4, test_size =0.2 ,droplist= []):
     X , inputFeatures= dropFeatures(droplist=droplist, X=X, inputFeatures=inputFeatures)
     y = data["IntClass"]
     y = y[:3349]
-    trainloader ,evalloader, testloader ,X_train ,X_eval, X_test ,  y_train, y_eval, y_test = preProcessingData(X=X, y=y,batch_size=batch_size, test_size=test_size,droplist=droplist)
+    trainloader , testloader ,X_train , X_test ,  y_train, y_test = preProcessingData(X=X, y=y,batch_size=batch_size, test_size=test_size,droplist=droplist)
 
-    return trainloader ,evalloader, testloader ,X_train, X_eval, X_test, y_train, y_eval, y_test , inputFeatures, outputFeatures, datasetName
+    return trainloader , testloader ,X_train, X_test, y_train, y_test , inputFeatures, outputFeatures, datasetName, features_names
