84bb75c9d563c36d41d03704119b13dd04c7fded
vellaro <r.vella@hotmail.de>
2023-06-07 11:41:53 +0200

    update

--- eval.py
+++ eval.py
@@ -8,7 +8,7 @@ import random
 
 from sklearn.metrics.pairwise import cosine_similarity
 
-def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_function, num_epochs, nameList, yList, inputFeatures):
+def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_function, num_epochs, nameList, yList, inputFeatures, random_indices_test):
     """
         parameters:
             model: an ititialised model with the same parameters, as for training
@@ -31,11 +31,13 @@ def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_f
         name = i
         ... (RUN ALL)
     """
+    dataPath= dirPath+ "Trainingresults/"
     #np.savez(dirPath + 'data.npz', exec(f'{name}acc = "{acc}"')) #exec :  executes the string that it gets 
-    np.savez(dirPath + 'data.npz', y_train = yList[0]) 
-    utils.appendToNPZ(dirPath + 'data.npz',"y_eval", yList[1])
-    utils.appendToNPZ(dirPath + 'data.npz',"y_test", yList[2])
-    utils.appendToNPZ(dirPath + 'data.npz',"inputFeatures", inputFeatures)
+    np.savez(dataPath + 'data.npz', y_test = yList[0]) 
+    #utils.appendToNPZ(dirPath + 'data.npz',"y_eval", yList[1])
+    #utils.appendToNPZ(dirPath + 'data.npz',"y_test", yList[2])
+    #utils.appendToNPZ(dirPath + 'data.npz',"y_test", yList[0])
+    utils.appendToNPZ(dataPath + 'Trainingresults.npz',"inputFeatures", inputFeatures)
 
     initialModel = model
     #initialModel.load_state_dict(torch.load(modelsDirPath + "/" +str(0)))
@@ -63,7 +65,7 @@ def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_f
         lossPerIterationList = []
 ### GRADS 
         gradientList= []#  gradients for each epoch on traoinset / for each iteration on the evaluationSet 
-
+        gradientList_iteration = []
 ### COSINE_SIMILARITY
         cosine_similarity_toInitialList = []
         cosine_similarity_toFinalList = []
@@ -115,40 +117,60 @@ def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_f
 
         #trainSetGradients = []
 
+        #counterTest = 0
 
         # load and loop through all model Iterations
-        for modelNumber,filename in enumerate(np.sort(list(eval(i) for i in modelsDirFiltered))): #(os.listdir(modelsDirPath)))): #
+        for modelNumber,filename in enumerate(np.sort(list(eval(i) for i in modelsDirFiltered))): #(os.listdir(modelsDirPath)))): # iterations time 
             model.load_state_dict(torch.load(modelsDirPath + "/" +str(filename)))
             model.eval()
 
             counter +=1
             tempIterationCounter +=1
 
-
             ### calculate grads
-            if name == "eval" or name == "train":
-                for inputs, labels in loader:
 
+            if True :#name == "eval" or name == "train":
+                gradAtitearation = [None] * len(yList[0] -1)
+                random_indices_testCOUNTER = 0
+                for inputs, labels in loader:
+                    #inputs = inputs.to(device)
+                    #labels = labels.to(device)
+#
+                    #inputs.requires_grad = True
+                    #optimizer.zero_grad()
+                    #outputs = model(inputs)
+                    #loss = loss_function(outputs, labels)
+                    #loss.backward()
+                    #optimizer.step()
+                    #optimizer.zero_grad()
+                    #outputs = model(inputs)
+                    ##
+                    #grads = utils.calc_grads(outputs, inputs)     
+                    #gradientList.append(grads.cpu())
                     inputs = inputs.to(device)
                     labels = labels.to(device)
+                    n_samples = 25
+                    stdev_spread = 0.2
 
-                    inputs.requires_grad = True
-                    optimizer.zero_grad()
-                    outputs = model(inputs)
-                    loss = loss_function(outputs, labels)
-                    loss.backward()
-                    optimizer.step()
-                    optimizer.zero_grad()
-                    outputs = model(inputs)
-                    #
-                    grads = utils.calc_grads(outputs, inputs)     
-                    gradientList.append(grads.cpu())
-            
+                    #inputs = F.softmax(input, dim=1) # softmax
 
+                    grad = utils.smooth_grad(inputs, n_samples, stdev_spread ,model) # for test dataset # grad for a Sample
+                    #print(np.shape(grad)) # 32 ,8 , batchsize , featuresize
+                    ##
+                    for i in range(len(inputs)):  # for batch size
+                        #print(random_indices_testCOUNTER)
+                        gradAtitearation[random_indices_test[random_indices_testCOUNTER]] = grad[i] # grads for the whole test dataset at given iteration 
+                        random_indices_testCOUNTER +=1
+                    #print(counterTest)
+                    #counterTest += 1
+                    #print(len(gradAtitearation))
+                gradientList_iteration.append(gradAtitearation)# 11,154,
+                
                 #get weights of model at iteration
                 #if name=="train":
                 #iterationWeightsF = utils.getWeights(finalModel)
-                #elif name == "eval":    
+                #elif name == "eval":
+
                 iterationWeights = utils.getWeights(model)
                 
                 #if (iterationWeightsF.all() !=iterationWeights.all()):
@@ -198,7 +220,7 @@ def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_f
 
                 for i, indices in enumerate(randomIndicesList):
                     random10WeightsList[i].append(iterationWeights[indices])
-
+                
             ### ACC / LOSS / PREDICTIONS_LIST
             predictionList = []     # from last epoch
 
@@ -232,6 +254,7 @@ def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_f
                     loss = np.mean(temp_Loss_epoch)
                     lossPerEpochList.append(loss)
 
+                    gradientList.append(gradAtitearation)  
                     #Progress
                     print("Progess: " + "{:.2f}".format( (modelNumber/ len(modelsDirFiltered) *100) )+"%")
                     print( name + " acc: " + "{:.2f}".format(acc*100) +"%") 
@@ -248,32 +271,43 @@ def doALLeval(model, modelsDirPath,dirPath, loaderList, device,optimizer, loss_f
         percentageWeightSignDifferences_toFinalList= np.array(weightSignDifferences_toFinalList)/ len(finalWeights)
 
         ### GRADIENT_MAGNITUDE
-        if name == "train" or name== "eval": 
+        if True:#name == "train" or name== "eval": 
+            #print(np.shape(gradientList))
             unpackedGradiends = utils.unpackingGradients(inputFeatures, gradientList)
             averagedGradientMagnitude = np.average(np.absolute(unpackedGradiends), axis=0) 
             gradientMagnitudePerFeature = np.absolute(unpackedGradiends)
+            ## per iteration:
+            unpackedGradiends_iteration = utils.unpackingGradients(inputFeatures, gradientList_iteration)
+            averagedGradientMagnitude_iteration = np.average(np.absolute(unpackedGradiends_iteration), axis=0) 
+            gradientMagnitudePerFeature_iteration = np.absolute(unpackedGradiends_iteration)
         
         # save all to NPZ
-        utils.appendToNPZ(dirPath+ "data.npz", name + "LossPerEpochList", lossPerEpochList)
-        utils.appendToNPZ(dirPath+ "data.npz", name + "LossPerIterationList", lossPerIterationList)
-        utils.appendToNPZ(dirPath+ "data.npz", name + "AccPerEpochList", accPerEpochList)
-        utils.appendToNPZ(dirPath+ "data.npz", name + "AccPerIterationList", accPerIterationList)
-        utils.appendToNPZ(dirPath+ "data.npz", name + "PredictionList", predictionList) # from last epoch
+        utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "LossPerEpochList", lossPerEpochList)
+        utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "LossPerIterationList", lossPerIterationList)
+        utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "AccPerEpochList", accPerEpochList)
+        utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "AccPerIterationList", accPerIterationList)
+        utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "PredictionList", predictionList) # from last epoch
 
-        if name == "train" or name == "eval":
-            utils.appendToNPZ(dirPath+ "data.npz", name + "Cosine_similarity_toInitialList", cosine_similarity_toInitialList) # Default always Per Iteration
-            utils.appendToNPZ(dirPath+ "data.npz", name + "Cosine_similarity_toFinalList", cosine_similarity_toFinalList)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "PercentageWeightSignDifferences_toInitialList", percentageWeightSignDifferences_toInitialList)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "PercentageWeightSignDifferences_toFinalList", percentageWeightSignDifferences_toFinalList)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "AbsoluteIterationWeightsList", absoluteIterationWeightsList) 
-            utils.appendToNPZ(dirPath+ "data.npz", name + "L2Dist_toInitialList", l2Dist_toInitialList)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "L2Dist_toFinalList", l2Dist_toFinalList)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "Random10WeightsList", random10WeightsList)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "GradientsPerFeature", unpackedGradiends)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "GradientMagnitudePerFeature", gradientMagnitudePerFeature)
-            utils.appendToNPZ(dirPath+ "data.npz", name + "AveragedGradientMagnitude", averagedGradientMagnitude)
+        if True:#name == "train" or name == "eval":
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "Cosine_similarity_toInitialList", cosine_similarity_toInitialList) # Default always Per Iteration
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "Cosine_similarity_toFinalList", cosine_similarity_toFinalList)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "PercentageWeightSignDifferences_toInitialList", percentageWeightSignDifferences_toInitialList)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "PercentageWeightSignDifferences_toFinalList", percentageWeightSignDifferences_toFinalList)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "AbsoluteIterationWeightsList", absoluteIterationWeightsList) 
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "L2Dist_toInitialList", l2Dist_toInitialList)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "L2Dist_toFinalList", l2Dist_toFinalList)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "Random10WeightsList", random10WeightsList)
+
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "GradientsPerSamplePerFeature", gradientList) # for every trainingEpoch we calc the grads on testDataset
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "GradientsPerFeature", unpackedGradiends)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "GradientMagnitudePerFeature", gradientMagnitudePerFeature)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "AveragedGradientMagnitude", averagedGradientMagnitude)
+            
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "GradientsPerSamplePerFeature_iteration", gradientList_iteration) # for every trainingIteration we calc the grads on testDataset
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "GradientsPerFeature_iteration", unpackedGradiends_iteration)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "GradientMagnitudePerFeature_iteration", gradientMagnitudePerFeature_iteration)
+            utils.appendToNPZ(dataPath+ "Trainingresults.npz", name + "AveragedGradientMagnitude_iteration", averagedGradientMagnitude_iteration)
 
        
     return None # just save to npz
-
 
